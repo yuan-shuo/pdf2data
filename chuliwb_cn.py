8acdee7fb1c76696caa3c6127e5232dc81e62b2a
unknown <369599207@qq.com>
2024-05-04 16:34:31 +0800

    change model

--- chuliwb_cn.py
+++ chuliwb_cn.py
@@ -1,5 +1,5 @@
 import torch
-from ltp import LTP
+from ltp import LTP, StnSplit
 
 # 默认 huggingface 下载，可能需要代理
 
@@ -15,13 +15,16 @@ if torch.cuda.is_available():
 # 自定义词表
 # ltp.add_word("汤姆去", freq=2)
 # ltp.add_words(["外套", "外衣"], freq=2)
+sents = StnSplit().split(
+    "1889年4月20日，希特勒出生于当时奥匈帝国的因河畔布劳瑙"
+    )
+print(sents)
 
-#  分词 cws、词性 pos、命名实体标注 ner、语义角色标注 srl、依存句法分析 dep、语义依存分析树 sdp、语义依存分析图 sdpg
-text = "我是交大的一名学生"
-# text = 'The CNN algorithm is mainly used to realize the recognition and feature extraction of target images'
-output = ltp.pipeline([text], tasks=["cws", "ner", "sdpg"])
+for text in sents:
+    #  分词 cws、词性 pos、命名实体标注 ner、语义角色标注 srl、依存句法分析 dep、语义依存分析树 sdp、语义依存分析图 sdpg
+    output = ltp.pipeline([text], tasks=["cws", "ner", "sdpg"])
 
-# 使用字典格式作为返回结果
-print(f"命名实体识别：{output.ner}")
-print(f"分词：{output.cws}")
-print(f"语义依存分析图：{output.sdpg}")
+    # 使用字典格式作为返回结果
+    print(f"命名实体识别：{output.ner}")
+    print(f"分词：{output.cws}")
+    print(f"语义依存分析图：{output.sdpg}")
